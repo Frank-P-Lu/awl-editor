f49fe6cc039a054e2113e8a824bab37616dfd28e
Frank Lu <franklu.99@outlook.com>
2026-08-14 05:53:57 +0900

    render: name workspace fit result in planner laws

--- src/render/plan/tests.rs
+++ src/render/plan/tests.rs
@@ -20,7 +20,7 @@ mod accessory_law;
 /// deliberately do without.
 mod panel_law;
 
-use super::overlay_header::fit_workspace_item_rows;
+use super::overlay_header::{WorkspaceRowFit, fit_workspace_item_rows};
 use super::overlay_rows::{
     OverlayRowPlanInput, PlanLine, fit_item_rows, fit_item_rows_after_px, plan_overlay_rows,
 };
