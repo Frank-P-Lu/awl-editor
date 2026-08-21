713a6bdf662ec1c5b3cba5b4a79b39385e10dc90
Frank Lu <franklu.99@outlook.com>
2026-08-22 07:00:34 +0900

    tests: bound Trash waiter oracle

--- src/app/files/close/tests.rs
+++ src/app/files/close/tests.rs
@@ -814,6 +814,8 @@ mod waiters {
     }
 
     fn assert_done(mut mine: UnixStream, path: &Path) {
+        mine.set_read_timeout(Some(std::time::Duration::from_millis(250)))
+            .unwrap();
         let mut line = String::new();
         BufReader::new(&mut mine).read_line(&mut line).unwrap();
         assert_eq!(line, crate::daemon::format_done(path));
